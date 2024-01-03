36abe4814bf361fadb65412d161e383d2b976676
boratechlife <boratechlife.com>
2024-01-03 18:54:58 +0300

    add back vercel

---
+++ .vercel/output/functions/_qwik-city.func/@qwik-city-plan.js
@@ -0,0 +1,1 @@
+import{o as e,p as r,q as t,n as l,r as n,m as o,t as u}from"./assets/@qwik-city-plan-c0f6f7be.js";export{e as basePathname,r as cacheModules,t as default,l as menus,n as routes,o as serverPlugins,u as trailingSlash};
---
+++ .vercel/output/functions/_qwik-city.func/entry.ssr.js
@@ -0,0 +1,1 @@
+import"./assets/@qwik-city-plan-c0f6f7be.js";import{r as a}from"./assets/entry.ssr-f2fcd07b.js";export{a as default};
